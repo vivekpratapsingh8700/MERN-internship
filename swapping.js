4338fa394e4f2c6f201a0169c394df34745febab
vivekpratapsingh8700 <139740196+vivekpratapsingh8700@users.noreply.github.com>
2023-07-18 10:16:50 +0530

    Add files via upload

---
+++ swapping.js
@@ -0,0 +1,13 @@
+let number1=10;
+let number2=20;
+
+console.log("num1= "+number1);
+console.log("num2= "+number2);
+let temp=number1;
+number1=number2;
+number2=temp;
+
+console.log("after swapping");
+console.log("num1= "+number1);
+console.log("num2= "+number2);
+
